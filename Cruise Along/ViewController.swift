eccc26aa3fdbc73b398706555608e5ca9d1612ca
Chad Rutherford <chad_rutherford@outlook.com>
2020-03-01 10:58:47 -0500

    Initial Commit

---
+++ Cruise Along/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cruise Along
+//
+//  Created by Chad Rutherford on 3/1/20.
+//  Copyright © 2020 Chad Rutherford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
